a62f96fb8f49798d3bc11b1e0a001035cfdd0c44
Micol <micolpa08@gmail.com>
2022-06-25 20:28:00 -0400

    Update index.php

--- index.php
+++ index.php
@@ -319,41 +319,6 @@
                         </div>
                     </div> -->
                 </div>
-                <div class="col-lg-6 pe-lg-5">
-                    <div class="progress-info info1">
-                        <h6 class="progress-tittle">PHP <span class="">80%</span></h6>
-                        <div class="progress">
-                            <div class="progress-bar progress-bar-striped gradient-1" role="progressbar"
-                                style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="progress-info info2">
-                        <h6 class="progress-tittle">HTML/CSS <span class="">90%</span>
-                        </h6>
-                        <div class="progress">
-                            <div class="progress-bar progress-bar-striped gradient-2" role="progressbar"
-                                style="width: 95%" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="progress-info info3">
-                        <h6 class="progress-tittle">JavaScript <span class="">60%</span></h6>
-                        <div class="progress">
-                            <div class="progress-bar progress-bar-striped gradient-3" role="progressbar"
-                                style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
-                            </div>
-                        </div>
-                    </div>
-                    <!-- <div class="progress-info info4 mb-0">
-                        <h6 class="progress-tittle">RESTful APIs <span class="">85%</span></h6>
-                        <div class="progress">
-                            <div class="progress-bar progress-bar-striped gradient-4" role="progressbar"
-                                style="width: 85%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
-                            </div>
-                        </div>
-                    </div> -->
-                </div>
                 <div class="col-lg-6 ps-lg-5 mt-lg-0 mt-5">
                     <div class="progress-info info1">
                         <h6 class="progress-tittle">RESTful APIs <span class="">85%</span></h6>
